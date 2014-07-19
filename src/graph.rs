e78ae55074d603511c7e454ec706ac2ab0854427
nham <hamann.nick@gmail.com>
2014-07-19 15:38:11 -0400

    Add a Graph trait so we can have one implementation of BFS. REname Graph type to UnGraph.

--- src/graph.rs
+++ src/graph.rs
@@ -1,75 +1,23 @@
 use std::collections::{HashSet, RingBuf, Deque};
+use std::collections::hashmap::SetItems;
 
-struct GraphError {
-    error_type: GraphErrorType,
-}
-
-impl GraphError {
-    fn invalid_index(ind: NodeIndex) -> GraphError {
-        GraphError { error_type: InvalidNodeIndex(ind) }
-    }
-}
-
-enum GraphErrorType {
-    InvalidNodeIndex(NodeIndex),
-}
-
-type NodeIndex = uint;
-
-struct Graph<S> {
-    nodes: Vec<Node<S>>,
-    adj: Vec<HashSet<NodeIndex>>,
-    num_nodes: uint,
-}
-
-#[deriving(Show)]
-struct Node<S> {
-    data: S,
-    index: NodeIndex,
-}
-
-impl<S> Graph<S> {
-    pub fn new() -> Graph<S> {
-        Graph { nodes: vec!(), adj: vec!(), num_nodes: 0 }
-    }
-
+trait Graph<T> {
     /// Insert a new node, returning its index
-    pub fn add_node(&mut self, val: S) -> NodeIndex {
-        let ind = self.num_nodes;
-        self.nodes.push(Node { data: val, index: ind });
-        self.adj.push(HashSet::new());
-        self.num_nodes += 1;
-        ind
-    }
+    fn add_node(&mut self, val: T) -> NodeIndex;
 
-    pub fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> Result<bool, GraphError> {
-        if i >= self.num_nodes {
-            Err(GraphError::invalid_index(i))
-        } else if j >= self.num_nodes {
-            Err(GraphError::invalid_index(j))
-        } else {
-            if self.adj[i].contains(&j) {
-                Ok(false)
-            } else {
-                self.adj.get_mut(i).insert(j);
-                self.adj.get_mut(j).insert(i);
-                Ok(true)
-            }
-        }
-    }
+    /// Insert a new edge, returning an error if one of the indices is invalid
+    /// and a result otherwise
+    fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> GraphResult<bool>;
 
-    pub fn degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
-        if ind >= self.num_nodes {
-            Err(GraphError::invalid_index(ind))
-        } else {
-            Ok(self.adj[ind].len())
-        }
-    }
+    fn adj<'a>(&'a self, i: NodeIndex) -> NodeIndices<'a>;
 
-    pub fn bfs(&self) -> Digraph<NodeIndex> {
+    /// Return the number of nodes in the graph
+    fn num_nodes(&self) -> uint;
+
+    fn bfs(&self) -> Digraph<NodeIndex> {
         let mut tree = Digraph::new();
 
-        if self.num_nodes == 0 {
+        if self.num_nodes() == 0 {
             return tree;
         }
 
@@ -87,7 +35,7 @@ impl<S> Graph<S> {
                     }
                     visited.insert(ind);
 
-                    for i in self.adj[ind].iter() {
+                    for i in self.adj(ind) {
                         if !visited.contains(i) {
                             ondeck.push_back((*i, Some(ind)));
                         }
@@ -99,43 +47,98 @@ impl<S> Graph<S> {
     }
 }
 
+struct GraphError {
+    error_type: GraphErrorType,
+}
+
+impl GraphError {
+    fn invalid_index(ind: NodeIndex) -> GraphError {
+        GraphError { error_type: InvalidNodeIndex(ind) }
+    }
+}
+
+type GraphResult<T> = Result<T, GraphError>;
+
+enum GraphErrorType {
+    InvalidNodeIndex(NodeIndex),
+}
+
+type NodeIndex = uint;
+
 #[deriving(Show)]
-struct Digraph<S> {
-    nodes: Vec<Node<S>>,
+struct Node<T> {
+    data: T,
+    index: NodeIndex,
+}
+
+/// Undirected graph. Allows loops.
+#[deriving(Show)]
+struct UnGraph<T> {
+    nodes: Vec<Node<T>>,
+    adj: Vec<HashSet<NodeIndex>>,
+    num_nodes: uint,
+}
+
+/// Directed graph. Allows loops.
+#[deriving(Show)]
+struct Digraph<T> {
+    nodes: Vec<Node<T>>,
     in_adj: Vec<HashSet<NodeIndex>>,
     out_adj: Vec<HashSet<NodeIndex>>,
     num_nodes: uint,
 }
 
-impl<S> Digraph<S> {
-    pub fn new() -> Digraph<S> {
-        Digraph { nodes: vec!(), in_adj: vec!(), out_adj: vec!(), num_nodes: 0 }
+impl<T> UnGraph<T> {
+    pub fn new() -> UnGraph<T> {
+        UnGraph { nodes: vec!(), adj: vec!(), num_nodes: 0 }
     }
 
-    /// Insert a new node, returning its index
-    pub fn add_node(&mut self, val: S) -> NodeIndex {
+    pub fn degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
+        if ind >= self.num_nodes {
+            Err(GraphError::invalid_index(ind))
+        } else {
+            Ok(self.adj[ind].len())
+        }
+    }
+}
+
+impl<T> Graph<T> for UnGraph<T> {
+    fn add_node(&mut self, val: T) -> NodeIndex {
         let ind = self.num_nodes;
         self.nodes.push(Node { data: val, index: ind });
-        self.in_adj.push(HashSet::new());
-        self.out_adj.push(HashSet::new());
+        self.adj.push(HashSet::new());
         self.num_nodes += 1;
         ind
     }
 
-    pub fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> Result<bool, GraphError> {
+    fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> GraphResult<bool> {
         if i >= self.num_nodes {
             Err(GraphError::invalid_index(i))
         } else if j >= self.num_nodes {
             Err(GraphError::invalid_index(j))
         } else {
-            if self.out_adj[i].contains(&j) {
+            if self.adj[i].contains(&j) {
                 Ok(false)
             } else {
-                self.out_adj.get_mut(i).insert(j);
-                self.in_adj.get_mut(j).insert(i);
+                self.adj.get_mut(i).insert(j);
+                self.adj.get_mut(j).insert(i);
                 Ok(true)
             }
         }
+    }
+
+    fn adj<'a>(&'a self, i: NodeIndex) -> NodeIndices<'a> {
+        NodeIndices::from_set(&self.adj[i])
+    }
+
+    fn num_nodes(&self) -> uint {
+        self.num_nodes
+    }
+}
+
+impl<T> Digraph<T> {
+    pub fn new() -> Digraph<T> {
+        Digraph { nodes: vec!(), in_adj: vec!(), out_adj: vec!(), num_nodes: 0 }
     }
 
     pub fn out_degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
@@ -153,36 +156,55 @@ impl<S> Digraph<S> {
             Ok(self.in_adj[ind].len())
         }
     }
+}
 
-    pub fn bfs(&self) -> Digraph<NodeIndex> {
-        let mut tree = Digraph::new();
+impl<T> Graph<T> for Digraph<T> {
+    fn add_node(&mut self, val: T) -> NodeIndex {
+        let ind = self.num_nodes;
+        self.nodes.push(Node { data: val, index: ind });
+        self.in_adj.push(HashSet::new());
+        self.out_adj.push(HashSet::new());
+        self.num_nodes += 1;
+        ind
+    }
 
-        if self.num_nodes == 0 {
-            return tree;
-        }
-
-        let mut visited = HashSet::new();
-        let mut ondeck = RingBuf::new();
-
-        ondeck.push_back((0, None));
-        loop {
-            match ondeck.pop_front() {
-                None => break,
-                Some((ind, parent)) => {
-                    tree.add_node(ind);
-                    if parent.is_some() {
-                        tree.add_edge(parent.unwrap(), ind);
-                    }
-                    visited.insert(ind);
-
-                    for i in self.out_adj[ind].iter() {
-                        if !visited.contains(i) {
-                            ondeck.push_back((*i, Some(ind)));
-                        }
-                    }
-                }
+    fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> GraphResult<bool> {
+        if i >= self.num_nodes {
+            Err(GraphError::invalid_index(i))
+        } else if j >= self.num_nodes {
+            Err(GraphError::invalid_index(j))
+        } else {
+            if self.out_adj[i].contains(&j) {
+                Ok(false)
+            } else {
+                self.out_adj.get_mut(i).insert(j);
+                self.in_adj.get_mut(j).insert(i);
+                Ok(true)
             }
         }
-        return tree;
+    }
+
+    fn adj<'a>(&'a self, i: NodeIndex) -> NodeIndices<'a> {
+        NodeIndices::from_set(&self.out_adj[i])
+    }
+
+    fn num_nodes(&self) -> uint {
+        self.num_nodes
+    }
+}
+
+struct NodeIndices<'a> {
+    indices: SetItems<'a, NodeIndex>,
+}
+
+impl<'a> Iterator<&'a NodeIndex> for NodeIndices<'a> {
+    fn next(&mut self) -> Option<&'a NodeIndex> {
+        self.indices.next()
+    }
+}
+
+impl<'a> NodeIndices<'a> {
+    fn from_set(set: &'a HashSet<NodeIndex>) -> NodeIndices<'a> {
+        NodeIndices { indices: set.iter() }
     }
 }
