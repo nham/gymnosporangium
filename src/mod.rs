5531fe50a86af6b192fb6f72264a92088957d59d
nham <hamann.nick@gmail.com>
2014-07-18 23:03:26 -0400

    Replace some uses of uint with NodeIndex. Change Digraph to use a separate adjacency list for inbound neighbors. Add methods for calculating out and in-degrees of digraphs

--- src/mod.rs
+++ src/mod.rs
@@ -34,7 +34,7 @@ impl<S> Graph<S> {
         self.num_nodes += 1;
     }
 
-    fn add_edge(&mut self, i: uint, j: uint) -> Result<bool, GraphError> {
+    fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> Result<bool, GraphError> {
         if i >= self.num_nodes {
             Err(GraphError::invalid_index(i))
         } else if j >= self.num_nodes {
@@ -50,36 +50,59 @@ impl<S> Graph<S> {
         }
     }
 
-    fn out_degree(&mut self, ind: NodeIndex) -> uint {
-        0u
+    fn degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
+        if ind >= self.num_nodes {
+            Err(GraphError::invalid_index(ind))
+        } else {
+            Ok(self.adj[ind].len())
+        }
     }
 }
 
 struct Digraph<S> {
     nodes: Vec<Node<S>>,
-    adj: Vec<HashSet<uint>>,
+    in_adj: Vec<HashSet<NodeIndex>>,
+    out_adj: Vec<HashSet<NodeIndex>>,
     num_nodes: uint,
 }
 
 impl<S> Digraph<S> {
     fn add_node(&mut self, val: S) {
         self.nodes.push(Node { data: val, index: self.num_nodes });
-        self.adj.push(HashSet::new());
+        self.in_adj.push(HashSet::new());
+        self.out_adj.push(HashSet::new());
         self.num_nodes += 1;
     }
 
-    fn add_edge(&mut self, i: uint, j: uint) -> Result<bool, GraphError> {
+    fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> Result<bool, GraphError> {
         if i >= self.num_nodes {
             Err(GraphError::invalid_index(i))
         } else if j >= self.num_nodes {
             Err(GraphError::invalid_index(j))
         } else {
-            if self.adj[i].contains(&j) {
+            if self.out_adj[i].contains(&j) {
                 Ok(false)
             } else {
-                self.adj.get_mut(i).insert(j);
+                self.out_adj.get_mut(i).insert(j);
+                self.in_adj.get_mut(j).insert(i);
                 Ok(true)
             }
+        }
+    }
+
+    fn out_degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
+        if ind >= self.num_nodes {
+            Err(GraphError::invalid_index(ind))
+        } else {
+            Ok(self.out_adj[ind].len())
+        }
+    }
+
+    fn in_degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
+        if ind >= self.num_nodes {
+            Err(GraphError::invalid_index(ind))
+        } else {
+            Ok(self.in_adj[ind].len())
         }
     }
 }
