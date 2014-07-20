7f388d551a37a12e329dadf3f692905f6038f0e3
nham <hamann.nick@gmail.com>
2014-07-20 15:59:05 -0400

    Change bfs to not traverse all the connected components.

--- src/traversal.rs
+++ src/traversal.rs
@@ -3,41 +3,27 @@ use std::collections::{HashMap, RingBuf, Deque};
 use graph::{Graph, NodeIndex, Digraph};
 
 /// Do a breadth-first search of the graph, returning the resulting breadth-
-/// first forest.
-fn bfs_forest<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Digraph<NodeIndex> {
-    let mut forest = Digraph::new();
+/// first tree (a tree on the connected component containing the stard node)
+fn bfs_tree<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Digraph<NodeIndex> {
+    let mut tree = Digraph::new();
 
     if g.num_nodes() == 0 {
-        return forest;
+        return tree;
     }
 
-    let mut unvisited = HashSet::new();
     let mut visited = HashSet::new();
     let mut discovered = RingBuf::new();
-
-    for i in g.node_indices() {
-        unvisited.insert(i);
-    }
 
     discovered.push_back((start, None));
     loop {
         match discovered.pop_front() {
-            None => {
-                if unvisited.len() == 0 {
-                    break;
-                } else {
-                    let another = unvisited.iter().next().unwrap();
-                    discovered.push_back((*another, None));
-                    continue;
-                }
-            },
+            None => break,
             Some((ind, parent)) => {
-                forest.add_node(ind);
+                tree.add_node(ind);
                 if parent.is_some() {
-                    forest.add_edge(parent.unwrap(), ind);
+                    tree.add_edge(parent.unwrap(), ind);
                 }
                 visited.insert(ind);
-                unvisited.remove(&ind);
 
                 for i in g.adj(ind) {
                     if !visited.contains(&i) {
@@ -47,6 +33,6 @@ fn bfs_forest<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Digraph<NodeIndex> {
             }
         }
     }
-    return forest;
+    return tree;
 }
 
