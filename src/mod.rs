b3edac411eae23f3196bda28c17a82f14fa85b7b
nham <hamann.nick@gmail.com>
2014-07-20 16:13:17 -0400

    Add a tree module

--- src/mod.rs
+++ src/mod.rs
@@ -2,6 +2,7 @@ pub use std::collections::{HashSet};
 
 mod graph;
 mod traversal;
+mod tree;
 
 fn main() {
 
--- src/tree.rs
+++ src/tree.rs
@@ -1,18 +1,38 @@
-struct TreeNode<T> {
+use {HashSet};
+
+type NodeIndex = uint;
+
+#[deriving(Show)]
+struct Node<T> {
     data: T,
-    children: Vec<TreeNode<T>>,
+    index: NodeIndex,
+    parent: Option<NodeIndex>,
 }
 
+#[deriving(Show)]
 pub struct Tree<T> {
-    root: Option<TreeNode<T>>
+    root: Option<NodeIndex>,
+    nodes: Vec<Node<T>>,
+    children: Vec<HashSet<NodeIndex>>,
+    num_nodes: uint,
 }
 
 impl<T> Tree<T> {
     pub fn new() -> Tree<T> {
-        Tree { root: None }
+        Tree { root: None, nodes: vec!(), children: vec!(), num_nodes: 0 }
     }
 
-    pub fn leaf(val: T) -> Tree<T> {
-        Tree { root: Some(TreeNode { data: val, children: vec!() }) }
+    pub fn with_root(val: T) -> Tree<T> {
+        let root = Node { data: val, index: 0, parent: None };
+        Tree { root: Some(0), nodes: vec!(root), children: vec!(), num_nodes: 1 }
+    }
+
+    pub fn add_child(&mut self, parent: NodeIndex, val: T) {
+        // TODO: check whether NodeIndex is valid?
+        let node = Node { data: val, index: self.num_nodes, parent: Some(parent) };
+        self.nodes.push(node);
+        self.children.push(HashSet::new());
+        self.children.get_mut(parent).insert(self.num_nodes);
+        self.num_nodes += 1;
     }
 }
