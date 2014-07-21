945c3a9965ab7f5d6a0cc8b3f63eed88d16b909e
nham <hamann.nick@gmail.com>
2014-07-20 22:54:42 -0400

    Fix BF-traversal, move DF-traversal to traversal module and fix it as well.

--- src/graph.rs
+++ src/graph.rs
@@ -17,39 +17,6 @@ pub trait Graph<T> {
     fn num_nodes(&self) -> uint;
 
     fn node_indices(&self) -> NodeIndices;
-
-    fn dfs(&self) -> Digraph<NodeIndex> {
-        let mut tree = Digraph::new();
-
-        if self.num_nodes() == 0 {
-            return tree;
-        }
-
-        let mut visited = HashSet::new();
-        let mut discovered = vec!();
-
-        discovered.push((0, None));
-        loop {
-            match discovered.pop() {
-                None => break,
-                Some((ind, parent)) => {
-                    tree.add_node(ind);
-                    if parent.is_some() {
-                        tree.add_edge(parent.unwrap(), ind);
-                    }
-                    visited.insert(ind);
-
-                    for i in self.adj(ind) {
-                        if !visited.contains(i) {
-                            discovered.push((*i, Some(ind)));
-                        }
-                    }
-                }
-            }
-        }
-        return tree;
-
-    }
 }
 
 struct GraphError {
--- src/traversal.rs
+++ src/traversal.rs
@@ -4,32 +4,46 @@ use std::collections::{HashMap, RingBuf, Deque};
 use graph::{Graph, NodeIndex};
 use tree::Tree;
 
-/// Do a breadth-first search of the graph, returning the resulting breadth-
-/// first tree (a tree on the connected component containing the stard node)
-fn bfs_tree<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Tree<NodeIndex> {
+/// Do a breadth-first traversal of the graph, returning the resulting breadth-
+/// first tree (a tree on the connected component containing the start node)
+///
+/// Pseudocode:
+///
+///    BFS(G, v):
+///        S = new queue
+///        S.insert(v)
+///        mark v as discovered
+///        while S is not empty:
+///            curr = S.pop()
+///            for all nodes w out-adjacent to curr:
+///                if w is not yet discovered
+///                    S.insert(w)
+///                    mark w as discovered
+fn bf_trav<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Tree<NodeIndex> {
     let mut tree = Tree::new();
 
     if g.num_nodes() == 0 {
         return tree;
     }
 
-    let mut visited = HashSet::new();
-    let mut discovered = RingBuf::new();
+    let mut discovered = HashSet::new();
+    let mut queue = RingBuf::new();
 
-    discovered.push_back((start, None));
+    queue.push_back((start, None));
+    discovered.insert(start);
     loop {
-        match discovered.pop_front() {
+        match queue.pop_front() {
             None => break,
             Some((ind, parent)) => {
                 match parent {
                     None => tree.add_root(ind),
                     Some(p_ind) => tree.add_child(p_ind, ind),
                 }
-                visited.insert(ind);
 
                 for i in g.adj(ind) {
-                    if !visited.contains(&i) {
-                        discovered.push_back((i, Some(ind)));
+                    if !discovered.contains(&i) {
+                        queue.push_back((i, Some(ind)));
+                        discovered.insert(i);
                     }
                 }
             }
@@ -38,3 +52,48 @@ fn bfs_tree<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Tree<NodeIndex> {
     return tree;
 }
 
+
+/// Do a depth-first traversal of the graph, returning the resulting depth-
+/// first tree
+///
+/// Pseudocode:
+///    DFS(G, v):
+///        S = new stack
+///        S.push(v)
+///        while S is not empty:
+///            curr = S.pop()
+///            if curr is not yet visited:
+///                add curr to visited
+///                for all nodes w out-adjacent to curr:
+///                    S.push(w)
+fn df_trav<T, G: Graph<T>>(g: &G, start: NodeIndex) -> Tree<NodeIndex> {
+    let mut tree = Tree::new();
+
+    if g.num_nodes() == 0 {
+        return tree;
+    }
+
+    let mut visited = HashSet::new();
+    let mut stack = vec!();
+
+    stack.push((start, None));
+    loop {
+        match stack.pop() {
+            None => break,
+            Some((ind, parent)) => {
+                if !visited.contains(&ind) {
+                    match parent {
+                        None => tree.add_root(ind),
+                        Some(p_ind) => tree.add_child(p_ind, ind),
+                    }
+                    visited.insert(ind);
+
+                    for i in g.adj(ind) {
+                        stack.push((i, Some(ind)));
+                    }
+                }
+            }
+        }
+    }
+    return tree;
+}
