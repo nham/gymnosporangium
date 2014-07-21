fcaf562b72aad3ffef4c3bdecffef600a2f5d1da
nham <hamann.nick@gmail.com>
2014-07-20 20:44:41 -0400

    Change UnGraph, Digraph to use hashmaps for nodes/adj lists instead of vectors.

--- src/graph.rs
+++ src/graph.rs
@@ -80,31 +80,39 @@ struct Node<T> {
 /// Undirected graph. Allows loops.
 #[deriving(Show)]
 pub struct UnGraph<T> {
-    nodes: Vec<Node<T>>,
-    adj: Vec<NodeIndexSet>,
+    nodes: HashMap<NodeIndex, Node<T>>,
+    adj: HashMap<NodeIndex, NodeIndexSet>,
     num_nodes: uint,
 }
 
 /// Directed graph. Allows loops.
 #[deriving(Show)]
 pub struct Digraph<T> {
-    nodes: Vec<Node<T>>,
-    in_adj: Vec<NodeIndexSet>,
-    out_adj: Vec<NodeIndexSet>,
+    nodes: HashMap<NodeIndex, Node<T>>,
+    in_adj: HashMap<NodeIndex, NodeIndexSet>,
+    out_adj: HashMap<NodeIndex, NodeIndexSet>,
     num_nodes: uint,
 }
 
 impl<T> UnGraph<T> {
     pub fn new() -> UnGraph<T> {
-        UnGraph { nodes: vec!(), adj: vec!(), num_nodes: 0 }
+        UnGraph { nodes: HashMap::new(), adj: HashMap::new(), num_nodes: 0 }
     }
 
     pub fn degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
-        if ind >= self.num_nodes {
+        if !self.adj.contains_key(&ind) {
             Err(GraphError::invalid_index(ind))
         } else {
-            Ok(self.adj[ind].len())
+            Ok( self.get_adj(ind).len() )
         }
+    }
+
+    pub fn get_node<'a>(&'a self, i: NodeIndex) -> &'a Node<T> {
+        self.nodes.find(&i).unwrap()
+    }
+
+    pub fn get_adj<'a>(&'a self, i: NodeIndex) -> &'a NodeIndexSet {
+        self.adj.find(&i).unwrap()
     }
 }
 impl<T: Clone> UnGraph<T> {
@@ -115,12 +123,12 @@ impl<T: Clone> UnGraph<T> {
 
         for (i, ind) in nodes.iter().enumerate() {
             ind_map.insert(ind, i);
-            new.add_node(self.nodes[*ind].data.clone());
+            new.add_node(self.get_node(*ind).data.clone());
         }
 
         // Here we're assuming that NodeIndex = uint. Not sure how to easily do otherwise
         for i in range(0, self.num_nodes) {
-            for j in self.adj[i].iter() {
+            for j in self.get_adj(i).iter() {
                 new.add_edge(i, *j);
             }
         }
@@ -132,30 +140,30 @@ impl<T: Clone> UnGraph<T> {
 impl<T> Graph<T> for UnGraph<T> {
     fn add_node(&mut self, val: T) -> NodeIndex {
         let ind = self.num_nodes;
-        self.nodes.push(Node { data: val, index: ind });
-        self.adj.push(HashSet::new());
+        self.nodes.insert(ind, Node { data: val, index: ind });
+        self.adj.insert(ind, HashSet::new());
         self.num_nodes += 1;
         ind
     }
 
     fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> GraphResult<bool> {
-        if i >= self.num_nodes {
+        if !self.nodes.contains_key(&i) {
             Err(GraphError::invalid_index(i))
-        } else if j >= self.num_nodes {
+        } else if !self.nodes.contains_key(&j) {
             Err(GraphError::invalid_index(j))
         } else {
-            if self.adj[i].contains(&j) {
+            if self.get_adj(i).contains(&j) {
                 Ok(false)
             } else {
-                self.adj.get_mut(i).insert(j);
-                self.adj.get_mut(j).insert(i);
+                self.adj.find_mut(&i).unwrap().insert(j);
+                self.adj.find_mut(&j).unwrap().insert(i);
                 Ok(true)
             }
         }
     }
 
     fn adj(&self, i: NodeIndex) -> NodeIndices {
-        NodeIndices::from_set(&self.adj[i])
+        FromIterator::from_iter(self.get_adj(i).iter().map(|&x| x))
     }
 
     fn num_nodes(&self) -> uint {
@@ -163,20 +171,23 @@ impl<T> Graph<T> for UnGraph<T> {
     }
 
     fn node_indices(&self) -> NodeIndices {
-        NodeIndices::from_node_vec(&self.nodes)
+        FromIterator::from_iter(self.nodes.keys().map(|&x| x))
     }
 }
 
 impl<T> Digraph<T> {
     pub fn new() -> Digraph<T> {
-        Digraph { nodes: vec!(), in_adj: vec!(), out_adj: vec!(), num_nodes: 0 }
+        Digraph { nodes: HashMap::new(),
+                  in_adj: HashMap::new(),
+                  out_adj: HashMap::new(),
+                  num_nodes: 0 }
     }
 
     pub fn out_degree(&mut self, ind: NodeIndex) -> Result<uint, GraphError> {
-        if ind >= self.num_nodes {
+        if !self.nodes.contains_key(&ind) {
             Err(GraphError::invalid_index(ind))
         } else {
-            Ok(self.out_adj[ind].len())
+            Ok(self.get_out_adj(ind).len())
         }
     }
 
@@ -184,8 +195,20 @@ impl<T> Digraph<T> {
         if ind >= self.num_nodes {
             Err(GraphError::invalid_index(ind))
         } else {
-            Ok(self.in_adj[ind].len())
+            Ok(self.get_in_adj(ind).len())
         }
+    }
+
+    pub fn get_node<'a>(&'a self, i: NodeIndex) -> &'a Node<T> {
+        self.nodes.find(&i).unwrap()
+    }
+
+    pub fn get_in_adj<'a>(&'a self, i: NodeIndex) -> &'a NodeIndexSet {
+        self.in_adj.find(&i).unwrap()
+    }
+
+    pub fn get_out_adj<'a>(&'a self, i: NodeIndex) -> &'a NodeIndexSet {
+        self.out_adj.find(&i).unwrap()
     }
 }
 
@@ -197,16 +220,16 @@ impl<T: Clone> Digraph<T> {
 
         for (i, ind) in nodes.iter().enumerate() {
             ind_map.insert(ind, i);
-            new.add_node(self.nodes[*ind].data.clone());
+            new.add_node(self.get_node(*ind).data.clone());
         }
 
         // Here we're assuming that NodeIndex = uint. Not sure how to easily do otherwise
         for i in range(0, self.num_nodes) {
-            for j in self.in_adj[i].iter() {
+            for j in self.get_in_adj(i).iter() {
                 new.add_edge(*j, i);
             }
 
-            for j in self.out_adj[i].iter() {
+            for j in self.get_out_adj(i).iter() {
                 new.add_edge(i, *j);
             }
         }
@@ -218,31 +241,31 @@ impl<T: Clone> Digraph<T> {
 impl<T> Graph<T> for Digraph<T> {
     fn add_node(&mut self, val: T) -> NodeIndex {
         let ind = self.num_nodes;
-        self.nodes.push(Node { data: val, index: ind });
-        self.in_adj.push(HashSet::new());
-        self.out_adj.push(HashSet::new());
+        self.nodes.insert(ind, Node { data: val, index: ind });
+        self.in_adj.insert(ind, HashSet::new());
+        self.out_adj.insert(ind, HashSet::new());
         self.num_nodes += 1;
         ind
     }
 
     fn add_edge(&mut self, i: NodeIndex, j: NodeIndex) -> GraphResult<bool> {
-        if i >= self.num_nodes {
+        if !self.nodes.contains_key(&i) {
             Err(GraphError::invalid_index(i))
-        } else if j >= self.num_nodes {
+        } else if !self.nodes.contains_key(&j) {
             Err(GraphError::invalid_index(j))
         } else {
-            if self.out_adj[i].contains(&j) {
+            if self.get_out_adj(i).contains(&j) {
                 Ok(false)
             } else {
-                self.out_adj.get_mut(i).insert(j);
-                self.in_adj.get_mut(j).insert(i);
+                self.out_adj.find_mut(&i).unwrap().insert(j);
+                self.in_adj.find_mut(&j).unwrap().insert(i);
                 Ok(true)
             }
         }
     }
 
     fn adj(&self, i: NodeIndex) -> NodeIndices {
-        NodeIndices::from_set(&self.out_adj[i])
+        FromIterator::from_iter(self.get_out_adj(i).iter().map(|&x| x))
     }
 
     fn num_nodes(&self) -> uint {
@@ -250,7 +273,7 @@ impl<T> Graph<T> for Digraph<T> {
     }
 
     fn node_indices(&self) -> NodeIndices {
-        NodeIndices::from_node_vec(&self.nodes)
+        FromIterator::from_iter(self.nodes.keys().map(|&x| x))
     }
 }
 
@@ -270,21 +293,13 @@ impl Iterator<NodeIndex> for NodeIndices {
     }
 }
 
-impl NodeIndices {
-    fn from_set(set: &NodeIndexSet) -> NodeIndices {
+impl FromIterator<NodeIndex> for NodeIndices {
+    fn from_iter<T: Iterator<NodeIndex>>(mut it: T) -> NodeIndices {
         let mut vec = vec!();
-        for &i in set.iter() {
+        for i in it {
             vec.push(i);
         }
         NodeIndices { indices: vec, curr: 0 }
     }
 
-    fn from_node_vec<T>(vec: &Vec<Node<T>>) -> NodeIndices {
-        let mut new = vec!();
-        for n in vec.iter() {
-            new.push(n.index);
-        }
-
-        NodeIndices { indices: new, curr: 0 }
-    }
 }
