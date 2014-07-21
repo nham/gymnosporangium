da1e71ca2c9f52a8f9971b2754d9d71c423c5b05
nham <hamann.nick@gmail.com>
2014-07-21 12:04:27 -0400

    Add some tests in main() for transpose and induced_subgraph

--- src/mod.rs
+++ src/mod.rs
@@ -21,11 +21,57 @@ fn main() {
     let mut h = Digraph::new();
 
     println!("{}", h);
-    h.add_node('a');
-    h.add_node('b');
-    h.add_node('c');
+    let a = h.add_node('a');
+    let b = h.add_node('b');
+    let c = h.add_node('c');
     println!("{}", h);
-    h.add_edge(0, 1);
-    h.add_edge(0, 2);
+    h.add_edge(a, b);
+    h.add_edge(a, c);
     println!("{}", h);
+
+    let d = h.add_node('d');
+    let e = h.add_node('e');
+    h.add_edge(d, a);
+    h.add_edge(e, a);
+    h.add_edge(d, e);
+
+    println!("-----------");
+    println!("{}", h);
+    println!("{}", h.transpose());
+    let mut nix_d = HashSet::new();
+    nix_d.insert(a);
+    nix_d.insert(b);
+    nix_d.insert(c);
+    nix_d.insert(e);
+    println!("{}", h.induced_subgraph(&nix_d));
+
+    println!("-----------");
+    let x: Digraph<()> = Digraph::new();
+    println!("{}", x);
+    println!("{}", x.transpose());
+    println!("-----------");
+
+    // testing induced_subgraph for an undirected graph
+    let mut y = Ungraph::new();
+    let a = y.add_node('a');
+    let b = y.add_node('b');
+    let c = y.add_node('c');
+    let d = y.add_node('d');
+    let e = y.add_node('e');
+    let f = y.add_node('f');
+    y.add_edge(a, b);
+    y.add_edge(a, d);
+    y.add_edge(a, e);
+    y.add_edge(b, c);
+    y.add_edge(c, e);
+    y.add_edge(d, e);
+    y.add_edge(d, f);
+    y.add_edge(e, f);
+    println!("{}", y);
+    let mut nix_ab = HashSet::new();
+    nix_ab.insert(c);
+    nix_ab.insert(d);
+    nix_ab.insert(e);
+    nix_ab.insert(f);
+    println!("{}", y.induced_subgraph(&nix_ab));
 }
